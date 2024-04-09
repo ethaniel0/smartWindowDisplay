52623880e31b6dea98acabe7b8f41297b75bec9e
Lilly Chiavetta <lilly.chiavetta21@gmail.com>
2024-04-09 13:11:11 -0400

    Made it "better" but it still doesn't work

--- server/index.js
+++ server/index.js
@@ -1,86 +1,90 @@
-const express = require('express')
-const app = express()
-const http = require('http');
-const { join } = require('path');
+const express = require("express");
+const app = express();
+const http = require("http");
+const { join } = require("path");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-const port = 3000
+const port = 3000;
 
-app.use(express.static('static'));
+app.use(express.static("static"));
 
 piSocket = null;
 userSocket = null;
-joinCode = ''
+joinCode = "";
 
-io.on('connection', (socket) => {
-    console.log('a user connected');
-    socket.on('message', (msg) => {
-        console.log(msg);
-        io.emit('message', msg + " we love Duke");
-    });
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("message", (msg) => {
+    console.log(msg);
+    io.emit("message", msg + " we love Duke");
+  });
 
-    // USER MESSAGES
-    socket.on('tryConnect', () => {
-        if (userSocket){
-            return socket.emit('tryConnect', 'already connected');
-        }
-      console.log('trying to connect');
-      socket.emit('tryConnect', 'received');
-      io.to('pi').emit('tryConnect');
-    });
+  // USER MESSAGES
+  socket.on("tryConnect", () => {
+    // if (userSocket) {
+    //   return socket.emit("tryConnect", "already connected");
+    // }
+    console.log("trying to connect");
+    socket.emit("tryConnect", "received");
+    io.to("pi").emit("tryConnect");
+  });
 
-    socket.on('joinCode', msg => {
-      console.log('join code', msg);
-        if (msg == joinCode) {
-          console.log('YAYAYYAAYAYYAY');
-            userSocket = socket;
-            socket.emit('joinCode', 'success');
-        }
-    })
+  socket.on("joinCode", (msg) => {
+    console.log("join code", msg);
+    if (msg == joinCode) {
+      console.log("YAYAYYAAYAYYAY");
+      userSocket = socket;
+      socket.emit("joinCode", "success");
+      io.to("pi").emit("joinCode", "success");
+      console.log("sent success");
+    } else {
+      socket.emit("joinCode", "fail");
+      io.to("pi").emit("joinCode", "fail");
+    }
+  });
 
-    socket.on('chooseProgram', msg => {
-        if (piSocket) {
-            io.to(piSocket).emit('chooseProgram', msg);
-        }
-    });
+  socket.on("chooseProgram", (msg) => {
+    if (piSocket) {
+      console.log("sending program", msg);
+      io.to(piSocket).emit("chooseProgram", msg);
+    }
+  });
 
-    // PI MESSAGES
+  // PI MESSAGES
 
-    socket.on('supersecretpimessage', msg => {
-        if (msg == "3.1415926535897932384626433832769") {
-            socket.join('pi');
-            socket.emit('supersecretpimessage', "success");
-            piSocket = socket;
-        }
-    })
+  socket.on("supersecretpimessage", (msg) => {
+    if (msg == "3.1415926535897932384626433832769") {
+      socket.join("pi");
+      socket.emit("supersecretpimessage", "success");
+      piSocket = socket;
+    }
+  });
 
-    socket.on('piJoinCode', msg => {
-      console.log("got a join code", msg)
-        if (piSocket && socket.id == piSocket.id) {
-          console.log('setting join code');
-            joinCode = msg;
-        }
-    });
+  socket.on("piJoinCode", (msg) => {
+    console.log("got a join code", msg);
+    if (piSocket && socket.id == piSocket.id) {
+      joinCode = msg;
+    }
+  });
 
-    socket.on('programList', msg => {
-        if (userSocket) {
-            io.to(userSocket).emit('programList', msg);
-        }
-    });
+  socket.on("programList", (msg) => {
+    if (userSocket) {
+      console.log("Recieved program list: " + msg);
+      io.to(userSocket).emit("programList", msg);
+    }
+  });
 
-    socket.on('disconnect', () => {
-        if (piSocket && socket.id == piSocket.id) {
-            piSocket = null;
-        }
-        else if (userSocket && socket.id == userSocket.id) {
-            userSocket = null;
-        }
-    })
+  socket.on("disconnect", () => {
+    if (piSocket && socket.id == piSocket.id) {
+      piSocket = null;
+    } else if (userSocket && socket.id == userSocket.id) {
+      userSocket = null;
+    }
+  });
 });
 
-
 server.listen(port, () => {
-  console.log(`Example app listening on port http://localhost:${port}`)
-})
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
